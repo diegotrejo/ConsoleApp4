fa8f8e26239b91df2b3402bb9431185b7b8c04e0
Diego Trejo <diego_trejo@hotmail.com>
2023-10-24 19:46:24 -0500

    Update Program.cs
    update 1

--- Program.cs
+++ Program.cs
@@ -39,11 +39,56 @@ var ceci = new Persona
     NroCedula = "7856348956"
 };
 
-var listaPersonas = new List<Persona> { pepe, ana, luis, ceci };
+var listaPersonas = new List<Persona> { 
+    pepe, 
+    ana, 
+    luis, 
+    ceci, 
+    //null, 
+    //null, 
+    new Persona {
+    Nombres = "JULIETA CRISTINA",
+    Apellidos = "CEVALLOS",
+    FechaNacimiento = new DateTime(1983, 8, 13),
+    Estatura = 1.59,
+    NroCedula = "2198390223"
+}};
+
+listaPersonas.Add(new Persona
+{
+    Nombres = "RICARDO",
+    Apellidos = "SALAZAR",
+    FechaNacimiento = new DateTime(1980, 6, 13),
+    Estatura = 1.75,
+    NroCedula = "892189090"
+});
+
+listaPersonas.Insert(2, new Persona
+{
+    Nombres = "JUAN CARLOS",
+    Apellidos = "PATIÑO",
+    FechaNacimiento = new DateTime(1987, 8, 23),
+    Estatura = 1.70,
+    NroCedula = "128390321"
+
+});
+
+
+listaPersonas.Remove(ceci);
+
+var masAlto = listaPersonas.Max( p => p.Estatura);
+var masBajo = listaPersonas.Min( p => p.Estatura);
+var promedio = listaPersonas.Average( p => p.Estatura);
+
+var rick = listaPersonas.Find(p => p.Nombres == "RICARDO");
+var personaMasAlta = listaPersonas.Find(p => p.Estatura == masAlto);
 
 foreach (var persona in listaPersonas)
-{ 
-    Console.WriteLine(persona.Datos());
+{
+    if (persona != null)
+    {
+        Console.WriteLine(persona.Datos());
+    }
 }
 
 Console.ReadLine();
