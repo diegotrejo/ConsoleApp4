65944ed11d09c0dceda8045878afcc550e874b1d
Diego Trejo <diego_trejo@hotmail.com>
2023-10-24 19:18:13 -0500

    Agregar archivos de proyecto.

---
+++ Persona.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Persona
+    {
+        public string Apellidos { get; set; }
+        public string Nombres { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public double Estatura { get; set; }
+        public string NroCedula { get; set; }
+
+        public void Despertar()
+        {
+            
+        }
+
+        public void Caminar(string direccion, double distancia, double velocidad)
+        { 
+        }
+
+        public string Datos()
+        {
+            var s = $"{NroCedula} - {Apellidos} {Nombres}, nacido el {FechaNacimiento.ToString("dd-MMM-yyyy")}, su estatura es: {Estatura}";
+            return s;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp4;
+
+Console.WriteLine("Hello, World!");
+
+
+var pepe = new Persona {
+     Nombres = "JOSE LUIS",
+     Apellidos = "ESPINOSA",
+     FechaNacimiento = new DateTime(1994, 08, 28),
+     Estatura = 1.69,
+     NroCedula = "1234567890"
+};
+
+var ana = new Persona
+{
+    Nombres = "ANA BELÈN",
+    Apellidos = "CISNEROS",
+    FechaNacimiento = new DateTime(1996, 9, 19),
+    Estatura = 1.55,
+    NroCedula = "0987654321"
+};
+
+var luis = new Persona
+{
+    Nombres = "LUIS ANIBAL",
+    Apellidos = "CONTRERAS",
+    FechaNacimiento = new DateTime(1985, 11, 23),
+    Estatura = 1.72,
+    NroCedula = "29028934"
+};
+
+var ceci = new Persona
+{
+    Nombres = "CECILIA DE LOURDES",
+    Apellidos = "PINEDA",
+    FechaNacimiento = new DateTime(1990, 5, 15),
+    Estatura = 1.65,
+    NroCedula = "7856348956"
+};
+
+var listaPersonas = new List<Persona> { pepe, ana, luis, ceci };
+
+foreach (var persona in listaPersonas)
+{ 
+    Console.WriteLine(persona.Datos());
+}
+
+Console.ReadLine();
